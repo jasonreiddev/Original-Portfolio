75a75035230150db9b7f4966022e98d05283f6f1
Jason Reid <jason.reid@netconstruct.co.uk>
2021-07-16 20:35:03 +0100

    Added Cloudinary

---
+++ src/pages/cloudinaryTest.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+
+
+const Images = () => {
+    const data = useStaticQuery(graphql`
+        query CloudinaryImages {
+            allCloudinaryMedia {
+              edges {
+                node {
+                  secure_url
+                }
+              }
+            }
+          }
+          `
+    );
+    const clImages = data.allCloudinaryMedia.edges;
+
+    return (
+        <div>
+          <div>
+            {clImages.map((image, index) => (
+                  <div key={`${index}-cl`}>
+                    <img src={image.node.secure_url} />
+                  </div>
+                ))
+            }
+          </div>
+        </div>
+      )
+  };
+
+  export default Images;
