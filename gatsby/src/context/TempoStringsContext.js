3ee07d287f910c614d12a3a119f92ed9f807b4ab
Jason Reid <jason.reid@netconstruct.co.uk>
2021-08-12 12:57:54 +0100

    Tempo-strings overhaul, using react context

---
+++ gatsby/src/context/TempoStringsContext.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const TempoStringsContext = React.createContext();
+
+export function TempoStringsProvider({children}) {
+  const [selected, setSelected] = useState([]);
+  return (
+    <TempoStringsContext.Provider value={[selected, setSelected]}>
+      {children}
+    </TempoStringsContext.Provider>
+  );
+}
+
+export default TempoStringsContext;
---
+++ gatsby/src/utils/useTempoStrings.js
@@ -0,0 +1,23 @@
+import {useContext} from 'react';
+import TempoStringsContext from '../context/TempoStringsContext';
+
+export default function useTempoStrings() {
+  const [selected, setSelected] = useContext(TempoStringsContext);
+
+  function addTempoString(addedTempoString) {
+    setSelected([...selected, addedTempoString]);
+  }
+
+  function removeTempoString(index) {
+    setSelected([
+      ...selected.slice(0, index),
+      ...selected.slice(index + 1),
+    ]);
+  }
+
+  return {
+    selected,
+    addTempoString,
+    removeTempoString,
+  };
+}
