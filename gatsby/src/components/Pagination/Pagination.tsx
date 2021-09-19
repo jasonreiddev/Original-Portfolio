0ddbe4944073fbb02d6b8ba288045cbc58d9b990
Jason Reid <jason.reid@netconstruct.co.uk>
2021-09-19 01:11:50 +0100

    Fix Prev styling pagination bug

--- gatsby/src/components/Pagination/Pagination.tsx
+++ gatsby/src/components/Pagination/Pagination.tsx
@@ -29,7 +29,7 @@ export const Pagination = ({
         <PaginationStyles>
           <Link
             title="Prev Page"
-            disabled={hasPrevPage}
+            disabled={!hasPrevPage}
             to={`${base}/${prevPage}`}
           >
             <AiOutlineLeft/><span className="word">Prev</span>
