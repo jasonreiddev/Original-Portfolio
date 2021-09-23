f84d3a9d6901bb95793101bf115a8e0acb098df5
Jason Reid <jason.reid@netconstruct.co.uk>
2021-09-23 14:51:58 +0100

    Fixed tempostrigs styles

--- gatsby/src/components/SelectedTempoStrings/SelectedTempoStrings.styles.tsx
+++ gatsby/src/components/SelectedTempoStrings/SelectedTempoStrings.styles.tsx
@@ -1,36 +1,5 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const ContainerStyles = styled.a`
+export const ContainerStyles = styled.a`
   cursor: pointer;
 `;
-
-interface SelectedTempoStringsProps{
-  selected?: any;
-  removeTempoString?: any;
-}
-
-export const SelectedTempoStrings = (
-    {selected,
-      removeTempoString,
-      /* tempoStrings,*/
-    }:SelectedTempoStringsProps ) => {
-  return (
-    <>
-      {selected && selected.map((tempoString, index) => {
-        // The following line would allow us to just pass IDs inside selected and get more data, not currently needed
-        // const tempoString = tempoStrings.find((tempoString) => tempoString === singleTempoString);
-        return (
-          <ContainerStyles key={index}
-            onClick={() => removeTempoString(index)}>
-            <p>&times; {
-              <span className="selected">
-                {tempoString}
-              </span>
-            }</p>
-          </ContainerStyles>
-        );
-      })}
-    </>
-  );
-};
